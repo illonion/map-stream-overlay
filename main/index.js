3e9b2945dd7b399f928bf1184dfad95fb153d168
Lawrence Zhou <60532041+illonion@users.noreply.github.com>
2024-11-05 13:42:52 +1100

    Add round name logic

--- main/index.js
+++ main/index.js
@@ -1,9 +1,20 @@
+// Get mappool
+const roundNameEl = document.getElementById("round-name");
+let allBeatmaps;
+async function getMappool() {
+    const response = await fetch("../_data/beatmaps.json");
+    const responseJson = await response.json();
+    allBeatmaps = responseJson.beatmaps;
+    roundNameEl.innerText = responseJson.roundName;
+}
+getMappool()
+
 // Socket Events
 // Credits: VictimCrasher - https://github.com/VictimCrasher/static/tree/master/WaveTournament
-const socket = new ReconnectingWebSocket("ws://" + location.host + "/ws")
-socket.onopen = () => { console.log("Successfully Connected") }
-socket.onclose = event => { console.log("Socket Closed Connection: ", event); socket.send("Client Closed!") }
-socket.onerror = error => { console.log("Socket Error: ", error) }
+const socket = new ReconnectingWebSocket("ws://" + location.host + "/ws");
+socket.onopen = () => { console.log("Successfully Connected"); }
+socket.onclose = event => { console.log("Socket Closed Connection: ", event); socket.send("Client Closed!"); }
+socket.onerror = error => { console.log("Socket Error: ", error); }
 
 // Team name information
 const teamNameLeftEl = document.getElementById("team-name-left");
