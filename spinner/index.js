723515ef6e5d619acc29dab7adc80aa7679d6574
Lawrence Zhou <60532041+illonion@users.noreply.github.com>
2025-01-26 16:35:50 +1100

    Add Set Spinner

--- spinner/index.js
+++ spinner/index.js
@@ -39,3 +39,52 @@ function clearPlayers() {
     allPlayers = []
     sidebarPlayerListEl.innerHTML = ""
 }
+
+// Set spinner
+const playerTextContainer = document.getElementById("player-text-container")
+let spinnerPlayers = []
+function setSpinner() {
+    spinnerPlayers = []
+    if (allPlayers.length === 0) return
+    while (spinnerPlayers.length < 9) {
+        spinnerPlayers = spinnerPlayers.concat(allPlayers)
+    }
+
+    playerTextContainer.innerHTML = ""
+
+    // Add first element to top invisible, elements 2-8 to display, then rest to bottom invisible
+    for (let i = 0; i < spinnerPlayers.length; i++) {
+        const playerDiv = document.createElement("div")
+        playerDiv.innerText = spinnerPlayers[i].username
+        switch (i) {
+            case 0:
+                playerDiv.classList.add("invisible-player", "player-0")
+                break
+            case 1:
+                playerDiv.classList.add("very-side-player", "player-1")
+                break
+            case 2:
+                playerDiv.classList.add("somewhat-side-player", "player-2")
+                break
+            case 3:
+                playerDiv.classList.add("somewhat-middle-player", "player-3")
+                break
+            case 4:
+                playerDiv.classList.add("player-4")
+                break
+            case 5:
+                playerDiv.classList.add("somewhat-middle-player", "player-5")
+                break
+            case 6:
+                playerDiv.classList.add("somewhat-side-player", "player-6")
+                break
+            case 7:
+                playerDiv.classList.add("very-side-player", "player-7")
+                break
+            default:
+                playerDiv.classList.add("invisible-player", "player-8")
+                break
+        }
+        playerTextContainer.append(playerDiv)
+    }
+}
