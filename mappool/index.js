9316526a246cbb457f99d980a9e5f0387714e55e
Lawrence Zhou <60532041+illonion@users.noreply.github.com>
2025-02-10 23:06:30 +1100

    Make side buttons work and preset tb details

--- mappool/index.js
+++ mappool/index.js
@@ -5,6 +5,9 @@ socket.onopen = () => console.log("Successfully Connected");
 socket.onclose = event => { console.log("Socket Closed Connection: ", event); socket.send("Client Closed!"); }
 socket.onerror = error => console.log("Socket Error: ", error);
 
+// TB card map idnividual
+const tbCardMapIndividual = document.getElementById("tb-card-map-individual")
+
 // Fetch data with Fetch API
 const sideBarMappoolEl = document.getElementById("side-bar-mappool")
 let currentBestOf = 0, currentFirstTo = 0, currentLeftStars = 0, currentRightStars = 0;
@@ -33,11 +36,22 @@ async function getMappool() {
         const sideBarMappoolButton = document.createElement("button")
         sideBarMappoolButton.classList.add("side-bar-button", "side-bar-mappool-button")
         sideBarMappoolButton.setAttribute("data-id", allBeatmaps[i].beatmapID)
+        sideBarMappoolButton.addEventListener("mousedown", mapClickEvent)
+        sideBarMappoolButton.addEventListener("contextmenu", function(event) {event.preventDefault()})
         sideBarMappoolButton.innerText = `${allBeatmaps[i].mod}${allBeatmaps[i].order}`
         sideBarMappoolEl.append(sideBarMappoolButton)
     }
+
+    // Set tb map info directly
+    const tbMap = findMapInMappool(2109961)
+    tbCardMapIndividual.children[1].style.backgroundImage = `url(${tbMap.imgURL})`
+    tbCardMapIndividual.children[4].children[0].innerText = tbMap.songName
+    tbCardMapIndividual.children[4].children[1].innerText = tbMap.artist
+    tbCardMapIndividual.children[6].children[0].innerText = tbMap.mapper
+    tbCardMapIndividual.children[7].children[0].innerText = `${Math.round(Number(tbMap.difficultyrating) * 100) / 100}*`
 }
-getMappool();
+getMappool()
+const findMapInMappool = beatmapId => allBeatmaps.find(beatmap => beatmap.beatmapID == beatmapId)
 
 // Create star display
 const teamStarsContainerRightEl = document.getElementById("team-stars-container-right")
@@ -59,6 +73,113 @@ function createStarDisplay() {
         if (fillStar) newStar.classList.add(side === "left"? "left-star" : "right-star");
         return newStar;
     }
+}
+
+// Map Click Event
+const mappoolSectionEl = document.getElementById("mappool-section")
+const mappoolSectionLeftEl = document.getElementById("mappool-section-left")
+const mappoolSectionRightEl = document.getElementById("mappool-section-right")
+let lastPickedTile
+function mapClickEvent(event) {
+    // Team
+    let team
+    if (event.button === 0) team = "red"
+    else if (event.button === 2) team = "blue"
+    if (!team) return
+
+    // Action
+    let action = "pick"
+    if (event.ctrlKey) action = "ban"
+    if (event.shiftKey) action = "protect"
+
+    // Find map
+    const map = findMapInMappool(this.dataset.id)
+    if (!map) return
+
+    // Make map card
+    const mapCard = document.createElement("div")
+    mapCard.classList.add("map-card")
+    
+    // Pick Image
+    const pickImage = document.createElement("img")
+    pickImage.setAttribute("src", `static/${team === "red" ? "left" : "right"}-pick.png`)
+
+    // Map Card Background
+    const mapCardBackground = document.createElement("div")
+    mapCardBackground.classList.add("map-card-background")
+    mapCardBackground.style.backgroundImage = `url("${map.imgURL}")`
+
+    // Map Details
+    const mapDetails = document.createElement("div")
+    mapDetails.classList.add("map-details")
+    // Song Name
+    const songName = document.createElement("div")
+    songName.classList.add("song-name")
+    songName.innerText = map.songName
+    // Song Artist
+    const songArtist = document.createElement("div")
+    songArtist.classList.add("song-artist")
+    songArtist.innerText = map.artist
+    mapDetails.append(songName, songArtist)
+
+    // Mod ID
+    const modId = document.createElement("div")
+    modId.classList.add("mod-id")
+    modId.classList.add(`mod-id-${map.mod.toLowerCase()}`)
+    // Mod Mask
+    const modMask = document.createElement("img")
+    modMask.classList.add("mod-mask")
+    modMask.setAttribute("src", "static/mask.png")
+    // Mod Text
+    const modText = document.createElement("div")
+    modText.classList.add("mod-text")
+    modText.innerText = `${map.mod.toUpperCase()}${map.order}`
+    modId.append(modMask, modText)
+
+    // Song Mapper
+    const songMapper = document.createElement("div")
+    songMapper.classList.add("song-mapper")
+    // Song Mapper Name
+    const songMapperName = document.createElement("span")
+    songMapperName.classList.add("map-detail-lime-bold")
+    songMapperName.innerText = map.mapper
+    songMapper.append("mapped by ", songMapperName)
+
+    // Star Rating
+    const starRating = document.createElement("div")
+    starRating.classList.add("star-rating")
+    // Star Rating Number
+    const starRatingNumber = document.createElement("span")
+    starRatingNumber.classList.add("map-detail-lime-bold")
+    starRatingNumber.innerText = `${Math.round(Number(map.difficultyrating) * 100) / 100}`
+    starRating.append("star rating // ", starRatingNumber)
+
+    // Map Card Action
+    const mapCardAction = document.createElement("div")
+    mapCardAction.classList.add("map-card-action")
+    // Map Card Action Tasks
+    const mapActionText = document.createElement("div")
+    if (action === "pick") {
+        mapActionText.classList.add("map-card-win-text")
+        mapActionText.innerText = "WIN"
+        mapCardAction.style.display = "none"
+    } else if (action === "ban") {
+        mapActionText.classList.add("map-card-ban-text", `map-card-colour-${team === "red"? "pink" : "blue"}`)
+        mapActionText.innerText = "B&"
+    } else if (action === "protect") {
+        mapActionText.classList.add("map-card-ban-text", `map-card-colour-${team === "red"? "pink" : "blue"}`)
+        mapActionText.innerText = "P#"
+    }
+    mapCardAction.append(mapActionText)
+    mapCard.append(pickImage, mapCardBackground, mapDetails, modId, songMapper, starRating, mapCardAction)
+    
+    const mappoolSectionElement = (team === "red")? mappoolSectionLeftEl : mappoolSectionRightEl
+    mappoolSectionElement.append(mapCard)
+
+    if (action === "pick") lastPickedTile = mapCard
+
+    // Scroll the mappool section
+    mappoolSectionEl.scrollTop = mappoolSectionEl.scrollHeight
 }
 
 // Change first pick bans
